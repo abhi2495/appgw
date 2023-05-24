ac7406649a41482e83741d0074e75ce82ebb10ef
Abhinaba Chakraborty <abhinaba.chakraborty@blueyonder.com>
2023-05-24 09:45:27 +0530

    trying to add subnets separately and also added public ip and app gateway

--- scripts/dev/main.tf
+++ scripts/dev/main.tf
@@ -28,20 +28,57 @@ resource "azurerm_virtual_network" "mlstudio" {
   resource_group_name = var.RESOURCE_GROUP_NAME
   address_space       = split(",", var.VNET_ADDRESS)
   tags                = var.TAGS
-
-  dynamic "subnet" {
-    for_each = var.SUBNETS
-    content {
-      name            = subnet.value["name"]
-      address_prefix  = setting.value["address_prefix"]
-    }
-  }
 }
 
-# resource "azurerm_public_ip" "example" {
-#   name                = "acceptanceTestPublicIp1"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
-#   allocation_method   = "Static"
-#   tags                = var.TAGS
-# }
+resource "azurerm_subnet" "appgw" {
+  name                 = var.APPGW_SUBNET_NAME
+  resource_group_name  = var.RESOURCE_GROUP_NAME
+  virtual_network_name = azurerm_virtual_network.mlstudio.name
+  address_prefixes     = [var.APPGW_SUBNET_ADDRESS_PREFIX]
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = var.AKS_SUBNET_NAME
+  resource_group_name  = var.RESOURCE_GROUP_NAME
+  virtual_network_name = azurerm_virtual_network.mlstudio.name
+  address_prefixes     = [var.AKS_SUBNET_ADDRESS_PREFIX]
+}
+
+resource "azurerm_public_ip" "mlstudio" {
+  name                = var.PUBLIC_IP_NAME
+  resource_group_name = var.RESOURCE_GROUP_NAME
+  location            = var.AZ_REGION
+  allocation_method   = var.PUBLIC_IP_ALLOCATION_METHOD
+  tags                = var.TAGS
+}
+
+locals {
+  frontend_port_name             = "appgw-feport"
+  frontend_ip_configuration_name = "appgw-feip"
+  gateway_ip_configuration_name = "appgw-ip-config"
+
+}
+
+resource "azurerm_application_gateway" "mlstudio" {
+  name                = var.APP_GATEWAY_NAME
+  resource_group_name = var.RESOURCE_GROUP_NAME
+  location            = var.AZ_REGION
+  tags                = var.TAGS
+  sku {
+    name     = var.APP_GATEWAY_SKU_NAME
+    tier     = var.APP_GATEWAY_SKU_TIER
+    capacity = var.APP_GATEWAY_SKU_CAPACITY
+  }
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.mlstudio.id
+  }
+  gateway_ip_configuration {
+    name      = local.gateway_ip_configuration_name
+    subnet_id = azurerm_subnet.appgw.id
+  }
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+}
--- scripts/dev/variables.tf
+++ scripts/dev/variables.tf
@@ -23,7 +23,42 @@ variable "VNET_ADDRESS" {
   description = "The address space that is used the virtual network. We can supply more than one address space using comma separated values."
 }
 
-variable "SUBNETS" {
-  description = "The subnets to be associated with the virtual network"
-  default = []
+variable "APPGW_SUBNET_NAME" {
+  description = "Name of the subnet to be used for Application Gateway"
+}
+
+variable "APPGW_SUBNET_ADDRESS_PREFIX" {
+  description = "Address prefix of the subnet to be used for Application Gateway"
+}
+
+variable "AKS_SUBNET_NAME" {
+  description = "Name of the subnet to be used for AKS Cluster"
+}
+
+variable "AKS_SUBNET_ADDRESS_PREFIX" {
+  description = "Address prefix of the subnet to be used for AKS Cluster"
+}
+
+variable "PUBLIC_IP_NAME" {
+  description = "Name of the Public IP."
+}
+
+variable "PUBLIC_IP_ALLOCATION_METHOD" {
+  description = "Defines the allocation method for this IP address. Possible values are Static or Dynamic."
+}
+
+variable "APP_GATEWAY_NAME" {
+  description = "The name of the Application Gateway."
+}
+
+variable "APP_GATEWAY_SKU_NAME" {
+  description = "The Name of the SKU to use for this Application Gateway. Possible values are Standard_Small, Standard_Medium, Standard_Large, Standard_v2, WAF_Medium, WAF_Large, and WAF_v2."
+}
+
+variable "APP_GATEWAY_SKU_TIER" {
+  description = "The Tier of the SKU to use for this Application Gateway. Possible values are Standard, Standard_v2, WAF and WAF_v2"
+}
+
+variable "APP_GATEWAY_SKU_CAPACITY" {
+  description = "The Capacity of the SKU to use for this Application Gateway. When using a V1 SKU this value must be between 1 and 32, and 1 to 125 for a V2 SKU. This property is optional if autoscale_configuration is set."
 }
